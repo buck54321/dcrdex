f54ccd6d5ed5c5583cb114f21d5a198978d21325
Brian Stafford <buck54321@gmail.com>
2021-11-10 17:19:37 -0600

    add AccountBalance method

--- server/asset/eth/eth_test.go
+++ server/asset/eth/eth_test.go
@@ -72,6 +72,8 @@ type testNode struct {
 	tx             *types.Transaction
 	txIsMempool    bool
 	txErr          error
+	acctBal        *big.Int
+	acctBalErr     error
 }
 
 func (n *testNode) connect(ctx context.Context, ipc string, contractAddr *common.Address) error {
@@ -114,6 +116,10 @@ func (n *testNode) swap(ctx context.Context, secretHash [32]byte) (*swap.ETHSwap
 
 func (n *testNode) transaction(ctx context.Context, hash common.Hash) (tx *types.Transaction, isMempool bool, err error) {
 	return n.tx, n.txIsMempool, n.txErr
+}
+
+func (n *testNode) accountBalance(ctx context.Context, addr common.Address) (*big.Int, error) {
+	return n.acctBal, n.acctBalErr
 }
 
 func tSwap(bn int64, locktime, value *big.Int, state SwapState, participantAddr *common.Address) *swap.ETHSwapSwap {
