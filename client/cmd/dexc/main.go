6baa3cc21c0eaec6ac38d5836360042e30e4bfd2
dev <xorzero777@gmail.com>
2023-04-10 15:46:30 +0800

    Basic client --  Passing BTC regtest live testing with harness

--- client/cmd/dexc/main.go
+++ client/cmd/dexc/main.go
@@ -22,6 +22,7 @@ import (
 	_ "decred.org/dcrdex/client/asset/btc"  // register btc asset
 	_ "decred.org/dcrdex/client/asset/dcr"  // register dcr asset
 	_ "decred.org/dcrdex/client/asset/doge" // register doge asset
+	_ "decred.org/dcrdex/client/asset/firo" // register firo asset
 	_ "decred.org/dcrdex/client/asset/ltc"  // register ltc asset
 	_ "decred.org/dcrdex/client/asset/zec"  // register zec asset
 
